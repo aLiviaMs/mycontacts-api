e64b789186c261ce59608dc39c4ce025652cc975
Livia Minucelli <liviawrk@gmail.com>
2023-02-01 19:24:00 -0300

    fix: resolve problem with deprecated lib uuidv4

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,12 +1,12 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
 const contacts = [
 	{
-		id: uuid(),
+		id: v4(),
 		name: 'John Doe',
 		email: 'john.doe@gmail.com',
 		phone: '555-555-5555',
-		category_id: uuid(),
+		category_id: v4(),
 	},
 ];
 
